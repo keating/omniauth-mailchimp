ea69f5490ac8ad70b7da987fb40cf81267c29182
keating <outofclouds@gmail.com>
2014-12-13 23:55:36 +0800

    second commit

--- lib/omniauth/strategies/mailchimp.rb
+++ lib/omniauth/strategies/mailchimp.rb
@@ -1,5 +1,5 @@
 require 'omniauth/strategies/oauth2'
-require 'multi_json'
+#require 'multi_json'
 
 module OmniAuth
   module Strategies
@@ -13,59 +13,61 @@ module OmniAuth
           :token_url => '/oauth2/token'
       }
 
-      def request_phase
-        req = Rack::Request.new(@env)
-        options.update(req.params)
-        super
-      end
-
-      uid {
-        raw_info["user_id"]
-      }
-
-      info do
-        contact = raw_info["contact"] || {}
-
-        {
-            :first_name => contact["fname"],
-            :last_name => contact["lname"],
-            :email => contact["email"]
-        }
-      end
-
-      extra do
-        {
-            :metadata => user_data,
-            :raw_info => raw_info
-        }
-      end
-
-      def raw_info
-        @raw_info ||= begin
-          data = user_data
-          endpoint = data["api_endpoint"]
-          apikey = "#{@access_token.token}-#{data['dc']}"
-          response = @access_token.get("#{endpoint}/2.0/helper/account-details?apikey=#{apikey}").parsed
-          if response["error"]
-            case response["code"]
-              when 109
-                fail!(:invalid_credentials, response["error"])
-            end
-          else
-            response
-          end
-        end
-      end
-
-      def user_data
-        @data ||= MultiJson.decode(@access_token.get("https://login.mailchimp.com/oauth2/metadata").body)
-      rescue ::OAuth2::Error => e
-        if e.response.status == 302
-          @data ||= MultiJson.decode(@access_token.get(e.response.headers['location']))
-        else
-          raise e
-        end
-      end
+      #def request_phase
+      #  req = Rack::Request.new(@env)
+      #  options.update(req.params)
+      #  super
+      #end
+      #
+      #uid {
+      #  raw_info["user_id"]
+      #}
+      #
+      #info do
+      #  contact = raw_info["contact"] || {}
+      #
+      #  {
+      #      :first_name => contact["fname"],
+      #      :last_name => contact["lname"],
+      #      :email => contact["email"]
+      #  }
+      #end
+      #
+      #extra do
+      #  {
+      #      :metadata => user_data,
+      #      :raw_info => raw_info
+      #  }
+      #end
+      #
+      #def raw_info
+      #  @raw_info ||= begin
+      #    data = user_data
+      #    endpoint = data["api_endpoint"]
+      #    apikey = "#{@access_token.token}-#{data['dc']}"
+      #    response = @access_token.get("#{endpoint}/2.0/helper/account-details?apikey=#{apikey}").parsed
+      #    if response["error"]
+      #      case response["code"]
+      #        when 109
+      #          fail!(:invalid_credentials, response["error"])
+      #      end
+      #    else
+      #      response
+      #    end
+      #  end
+      #end
+      #
+      #def user_data
+      #  @data ||= MultiJson.decode(@access_token.get("https://login.mailchimp.com/oauth2/metadata").body)
+      #rescue ::OAuth2::Error => e
+      #  if e.response.status == 302
+      #    @data ||= MultiJson.decode(@access_token.get(e.response.headers['location']))
+      #  else
+      #    raise e
+      #  end
+      #end
     end
   end
 end
+
+OmniAuth.config.add_camelization 'mailchimp', 'Mailchimp'
